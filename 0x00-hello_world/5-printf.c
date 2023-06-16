09dbd83afeaff6921157b1e8d27e743a412c6f72
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-16 07:39:51 -0700

    fixed bug

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * void - print "string"
  *
- * Description: This is the first function of the program
+ * @Description: This is the first function of the program
  * Return: retuns 0 shown succesful execution
  */
 int main(void)
