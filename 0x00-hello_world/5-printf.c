18811ef902a507497e777831b13c73c6b00064fe
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-16 07:37:38 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * void - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
