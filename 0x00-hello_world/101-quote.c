f9a041df8a20f1ba08a04fa225db73062e45b242
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-16 11:04:38 -0700

    last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{	char message = "and that piece of art is useful" - Dora Korpar, "
+"2015 - 10 - 19\n";
+	write(2, message, 59)
+	return (1);
+}
