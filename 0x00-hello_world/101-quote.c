93db8f52b097c388cec5711851aa197153548aab
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-16 11:58:52 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  * Return: retuns 0 shown succesful execution
  */
 int main(void)
-{	fput("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19\n", stdout);
+{	fputs("and that piece of art is useful\" - 
+Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
 /**
- * void - print "string"
+ * main - print "string"
  *
- * Description: This the main function of the program 
- * It prints specific sequence of string with 'puts' function
- * Return: return 0 shows successful execution
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
  */
 int main(void)
-{
-	puts("Programming is like building a multilingual puzzle\n");
+{	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
