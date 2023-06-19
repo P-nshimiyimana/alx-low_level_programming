4181311981dffec963afead32b37c6c45aa09271
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 07:03:12 -0700

    worked task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwyz";
+	int i;
+
+	for (i=0; i<26; i++)
+	{
+	putchar(alp[i]);
+	}
+	return (0);
+}
