a986ba262968d049b77078380ee98379b09883de
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 08:17:43 -0700

    worked task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
