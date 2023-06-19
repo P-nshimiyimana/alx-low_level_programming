6bd09efb20e6ab8fc0d5d00557ab919c5e7e7028
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 08:03:58 -0700

    worked task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
