22c9db73af24d7020fc10b4ef56d465918ca0149
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 10:36:55 -0700

    re corrected bugs

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 100; j++)
 	{
 	for (j = 0; j < 100; j++)
 	{
@@ -23,11 +23,11 @@ int main(void)
 	putchar(' ');
 	putchar((j / 10) + 48);
 	putchar((j % 10) + 48);
-	}
-	if (i != 98; || j != 99)
+	if (i != 98 || j != 99)
 	{
 	putchar(',');
 	putchar(' ');
+
 	}
 	}
 	}
@@ -35,3 +35,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
