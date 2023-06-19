8f7641dab58f0207e88c7185548ba128e028fa42
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 08:24:48 -0700

    worked task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+	putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
