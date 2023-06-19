ee308a4b8e07d7f225f8f4330efd89cfb73b4dce
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 08:47:02 -0700

    worked task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	int n, m;
+	
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+                {
+			if (n > m)
+			{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+                }
+	}
+	putchar('\n');
+	return (0);
+}
