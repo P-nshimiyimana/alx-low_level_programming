d6c1f29f1df80c5457c5e1efe29d8434ede973d7
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 07:22:17 -0700

    worked task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+        putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
