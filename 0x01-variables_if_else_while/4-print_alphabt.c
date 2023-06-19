84add2ba0f8aeaa8ea619af18781279da9ce26dc
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-19 07:54:21 -0700

    worked task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - print "string"
+ *
+ * Description: This is the first function of the program
+ * Return: retuns 0 shown succesful execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 90; i < 123; i++)
+	{
+	if (i != 101 && i = 113)
+	{
+	putchar(i);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
