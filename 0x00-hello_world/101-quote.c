bbf3742eb58f78568ec483e52389991b14edb3f8
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-20 09:27:33 -0700

    fixed bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 */
 int main(void)
 {       fputs("and that piece of art is useful\" - Dora ", stdout);
-        fputs("Korpar, 2015-10-19\n", stdout);
-        return (1);
+	fputs("Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
