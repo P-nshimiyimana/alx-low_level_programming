e5bd47b8073e2e5864e105961a501e3ea8937179
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-20 09:24:35 -0700

    fixed error regarding white space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print "string"
- *
- * Description: This is the first function of the program
- * Return: retuns 0 shown succesful execution
- */
+* main - A c program that prints a line to the standard error
+*
+* Return: 1 (Success)
+*/
 int main(void)
-{	fputs("and that piece of art is useful\" - 
-Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+{       fputs("and that piece of art is useful\" - Dora ", stdout);
+        fputs("Korpar, 2015-10-19\n", stdout);
+        return (1);
 }
