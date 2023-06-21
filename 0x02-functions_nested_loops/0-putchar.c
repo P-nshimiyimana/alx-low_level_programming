304ff8f5574779232e45b52cb93a5eb1237477fe
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 04:52:55 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
 #include "main.h"
-
 /**
- * main - writes the character c to stdout
- * @c: The character to print
+ * main - writes the character to stdout
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
 {
