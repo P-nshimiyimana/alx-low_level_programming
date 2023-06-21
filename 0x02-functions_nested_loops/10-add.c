67a077be88fb1dd84aa5644e9c8909d07e3645c0
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 13:20:17 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - print lower case
+ * @n1: hdhjsh
+ * @n2: hsjfh
+ * Return: 0 return successful executio
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
