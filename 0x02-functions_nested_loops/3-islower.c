ede02f4049eaaebf9b1ae9b261f9de818a933a8f
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 08:48:37 -0700

    checked lower cases

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower case
+ *
+ * Return: 0 return successful executio
+ */
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+	return (1);
+	}
+	else 
+	{
+	return (0);
+	}
+}
