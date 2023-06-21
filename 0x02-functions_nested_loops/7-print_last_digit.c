d61d434105d3e5434aa23e8e37be0793fbde86e6
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 11:29:19 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print lower case
+ * @n: The character to print
+ * Return: 0 return successful executio
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+	last = -last;
+	} 
+	{
+	_putchar (last + '0');
+	return last;
+	}
+}
