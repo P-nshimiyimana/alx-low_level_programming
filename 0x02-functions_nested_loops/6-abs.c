db8edd2cf33d36321d7c6319f8b06f71e8b68c07
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 10:57:52 -0700

    computed absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print lower case
+ * @c: The character to print
+ * Return: 0 return successful executio
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+}
