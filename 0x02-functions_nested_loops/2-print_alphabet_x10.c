25aa0c68e539f078e0bc0c5acc73a3ae5ad54c7b
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-21 06:48:46 -0700

    printed lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowcase ten times
+ *
+ * Return: 0 shows successful execution
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int c;
+	i = 0;
+
+	while (i < 10)
+	{
+	for (c = 97; c <= 122; c++)
+	{
+	_putchar (c);
+	}
+	_putchar ('\n');
+	i++;
+	}
+}
