fd13034715c60d34d5068685715186b2484e986d
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-22 10:49:54 -0700

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,4 +2,6 @@
 #define MAIN_HEADER_FILE
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
+int main(void);
+int convert_day(int month, int day);
 #endif
