7e42877c8a3e379a57270381ea2dc602fd80f238
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-23 01:26:39 -0700

    commmit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,10 @@
 /**
  * _isdigit - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  */
 int _isdigit(int c)
-{
+{	
 	if ((c >= 80 && c <= 89))
 	{
 	return (1);
