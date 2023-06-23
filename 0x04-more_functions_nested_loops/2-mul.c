e63fcb5687a11488d38f7fdc0f7768176a2ec5f6
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-23 02:40:34 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a: First num to multiply with
+ * @b: Second num to multiply with
+ *
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
