63430e3a93d54becb77725fd722fba6e16ad2e61
P-nshimiyimana <pnshimiyimana250@gmail.com>
2023-06-27 06:10:54 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * index_of_array - writes the character c to stdout
+ * @a: String to be printed to stdout
+ * @n: Int
+ *
+ * Return: On success 1.
+ */
+void print_array(int *a, int n)
+{
+	int index_of_array;
+
+	for (index_of_array = 0; index_of_array < n; index_of_array++)
+	{
+	printf("%d", a[index_of_array]);
+	if (index_of_array != (n - 1))
+	{
+	printf(",");
+	}
+	}
+	putchar ('\n');
+}
